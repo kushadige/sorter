c6d18ccbfbdafd68764fa1c8bc9191bc875ccb1a
Oğuzhan <oguzhankuslar@Oguzhan-MacBook-Air.local>
2023-08-20 07:07:21 +0300

    update index

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ const numbersCollection = new NumbersCollection([24, 52, -5, -123, 0]);
 const charactersCollection = new CharactersCollection("oguzhan");
 
 const sorter1 = new Sorter(numbersCollection);
+sorter1.bubble();
 console.log(sorter1.collection);
 
 const sorter2 = new Sorter(charactersCollection);
+sorter1.bubble();
 console.log(sorter2.collection);
